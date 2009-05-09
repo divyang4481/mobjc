76e0da21d979c3deaadc9f95a6f2c3f4ec0ff0ec
jesjones@mindspring.com <jesjones@mindspring.com@41438ad4-a222-11dd-9c66-c9f8973bfffa>
2009-05-09 00:27:02 +0000

    More thread model work.

--- trunk/source/helpers/ThreadModel.cs
+++ trunk/source/helpers/ThreadModel.cs
@@ -26,21 +26,27 @@ namespace MObjc.Helpers
 	public enum ThreadModel
 	{
 		// The code may execute multiple threads concurrently without user locking 
-		// (this is the default for code in the System/Mono namespaces).
+		// (this is the default for code in the System/Mono namespaces). For example 
+		// immutable types like System.String.
 		Concurrent = 0x0000,
 		
 		// The code may execute under multiple threads, but only if the execution is 
-		// serialized (e.g. by user level locking).
+		// serialized (e.g. by user level locking). For example, System.Collections.
+		// Generic.Dictionary.
 		Serializable = 0x0001,
 		
 		// The code runs under an arbitrary thread, but cannot be used concurrently.
+		// For example, a database connection type.
 		SingleThread = 0x0002,
 		
 		// The code may run safely only under the main thread (this is the default 
-		// for code outside the System/Mono namespaces).
+		// for code outside the System/Mono namespaces). For example, the Initialize 
+		// method of a concurrent Log type might be main thread.
 		MainThread = 0x0003,
 		
-		// The code runs under a single thread with a user specified name.
+		// The code runs under a single thread with a user specified name (e.g. the 
+		// method invoked by a timer) or the code has no name but is intended to be
+		// used with a named thread (e.g. the timer's delegate type). 
 		NamedThread = 0x0004,
 		
 		// Or this with the above for the rare cases where the code cannot be shown 
@@ -69,9 +75,6 @@ namespace MObjc.Helpers
 		
 		public ThreadModelAttribute(ThreadModel model)
 		{
-			if (model == ThreadModel.NamedThread)
-				throw new ArgumentException("Use the string constructor to decorate a named thread.");
-			
 			if (model == ThreadModel.AllowEveryCaller)
 				throw new ArgumentException("Or AllowEveryCaller with one of the other enum values.");
 			
