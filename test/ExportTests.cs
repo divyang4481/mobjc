66707f74271e0b7a1f395c559ab69967b0067ea4
jesjones@mindspring.com <jesjones@mindspring.com@41438ad4-a222-11dd-9c66-c9f8973bfffa>
2009-06-16 19:24:01 +0000

    Whitespace.

--- test/ExportTests.cs
+++ test/ExportTests.cs
@@ -48,37 +48,37 @@ public class ExportTests
 	[SetUp]
 	public void Setup()
 	{
-		GC.Collect(); 				
+		GC.Collect();
 		GC.WaitForPendingFinalizers();
 	}
 	
 	[Test]
-	public void NewTest() 
+	public void NewTest()
 	{
 		Subclass1 instance = Subclass1.makeDefault();
-	
+		
 		instance.Call("initValue");
-
+		
 		int value = (int) instance.Call("getValue");
 		Assert.AreEqual(100, value);
 	}
-
+	
 	[Test]
-	public void CreateTest() 
-	{	
+	public void CreateTest()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
-
+		
 		instance.Call("initValue");
-
+		
 		int value = (int) instance.Call("getValue");
 		Assert.AreEqual(100, value);
 	}
 	
 	[Test]
-	public void ManagedExceptionTest() 
-	{	
+	public void ManagedExceptionTest()
+	{
 		NSObject instance = new Class("Subclass1").Call("alloc").Call("init").To<NSObject>();
-
+		
 		try
 		{
 			Managed.LogException = (e) => {};
@@ -96,13 +96,13 @@ public class ExportTests
 	}
 		
 	[Test]
-	public void OverrideTest() 
-	{		
+	public void OverrideTest()
+	{
 		NSObject data = (NSObject) new Class("PrettyData").Call("alloc").Call("init");
-
+		
 		int value = (int) data.Call("get33");
 		Assert.AreEqual(33, value);
-
+		
 		NSObject istr = (NSObject) data.Call("description");
 		string str = Marshal.PtrToStringAuto((IntPtr) istr.Call("UTF8String"));
 		Assert.AreEqual("pretty: <>", str);
@@ -110,62 +110,62 @@ public class ExportTests
 	
 	[Test]
 	[ExpectedException(typeof(InvalidCallException))]
-	public void BadSelector() 
+	public void BadSelector()
 	{
 		NSObject data = (NSObject) new Class("PrettyData").Call("alloc").Call("init");
-
+		
 		data.Call("initWithUTF8String::", Marshal.StringToHGlobalAuto("hey"));
 	}
-
+	
 	[Test]
-	public void BaseTest() 
+	public void BaseTest()
 	{
 		NSObject str = (NSObject) new Class("MyBase").Call("alloc").Call("init");
-
+		
 		// can call NSString methods
 		sbyte b = (sbyte) str.Call("boolValue");
 		Assert.AreEqual(0, b);
-
+		
 		// can call new MyBase methods
 		int i = (int) str.Call("get33");
 		Assert.AreEqual(33, i);
-
+		
 		// can call overriden NSString methods
 		i = (int) str.Call("integerValue");
 		Assert.AreEqual(43, i);
 	}
-
+	
 	[Test]
-	public void MyDerived() 
+	public void MyDerived()
 	{
 		NSObject str = (NSObject) new Class("MyDerived").Call("alloc").Call("init");
-
+		
 		// can call NSString methods
 		sbyte b = (sbyte) str.Call("boolValue");
 		Assert.AreEqual(0, b);
-
+		
 		// can call MyBase methods
 		int i = (int) str.Call("get33");
 		Assert.AreEqual(33, i);
-
+		
 		// can call new MyDerived methods
 		i = (int) str.Call("get63");
 		Assert.AreEqual(63, i);
-
+		
 		// can call overriden NSString methods
 		i = (int) str.Call("intValue");
 		Assert.AreEqual(73, i);
-
+		
 		// can call overriden MyBase methods
 		i = (int) str.Call("integerValue");
 		Assert.AreEqual(74, i);
 	}
-
+	
 	[Test]
-	public void IVarTest() 
-	{	
+	public void IVarTest()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
-
+		
 		// ivars start out null
 		NSObject data = instance["myData"];
 		Assert.IsTrue(data == null);
@@ -179,92 +179,92 @@ public class ExportTests
 		
 		// the ref count of the value should be incremented when it is
 		// assigned to an ivar
-		Assert.AreEqual(count + 1, str.retainCount());		
-
+		Assert.AreEqual(count + 1, str.retainCount());
+		
 		// and we can get the value we set
 		NSObject result = instance["myData"];
-		Assert.AreEqual((IntPtr) str, (IntPtr) result);		
+		Assert.AreEqual((IntPtr) str, (IntPtr) result);
 	}
-
+	
 	[Test]
 	[ExpectedException(typeof(ArgumentException))]
-	public void BadIVarGetTest() 
-	{	
+	public void BadIVarGetTest()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
-
+		
 		NSObject data = instance["xxx"];
 		Assert.IsTrue(data != null);
 	}
-
+	
 	[Test]
 	[ExpectedException(typeof(ArgumentException))]
-	public void BadIVarSetTest() 
-	{	
+	public void BadIVarSetTest()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
-
+		
 		instance["xxx"] = instance;
 	}
-
+	
 	[Test]
-	public void UShortArg() 
-	{	
+	public void UShortArg()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
-
+		
 		ushort n = (ushort) instance.Call("TakeUInt162", (ushort) 12);
 		Assert.AreEqual(22, n);
 	}
 	
 	[Test]
-	public void StringArg() 
-	{	
+	public void StringArg()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
-
+		
 		NSString n = (NSString) instance.Call("TakeString", NSString.Create("hmm"));
 		Assert.AreEqual("hmmhmm", n.ToString());
 	}
 	
 	[Test]
-	public void ObjectArg1() 
-	{	
+	public void ObjectArg1()
+	{
 		Subclass1 x = Subclass1.make(13);
 		Subclass1 y = Subclass1.make(1);
-
+		
 		int n = (int) x.Call("TakeBase", y);
 		Assert.AreEqual(3, n);
 	}
 	
 	[Test]
-	public void ObjectArg2() 
-	{	
+	public void ObjectArg2()
+	{
 		NSObject x = Subclass1.make(13);
 		NSObject y = Subclass1.make(1);
-
+		
 		int n = (int) x.Call("TakeBase", y);
 		Assert.AreEqual(3, n);
 	}
 	
 	[Test]
-	public void DerivedArg() 
-	{	
+	public void DerivedArg()
+	{
 		Subclass1 x = Subclass1.make(13);
 		NSString s = NSString.Create("hey");
-
+		
 		NSString t = x.Call("concat", s, s).To<NSString>();
 		Assert.AreEqual("heyhey", t.description());
 	}
 	
 	[Test]
-	public void ObjectResult() 
-	{	
+	public void ObjectResult()
+	{
 		NSObject instance = (NSObject) new Class("PrettyData").Call("alloc").Call("init");
-
+		
 		string s = (string) instance.description();
 		Assert.IsTrue(s.StartsWith("pretty: "));
 	}
 		
 	[Test]
-	public void StructTest() 
-	{	
+	public void StructTest()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
 		
 		NSRange range = new NSRange();
@@ -272,24 +272,24 @@ public class ExportTests
 		range.length = 3;
 		
 		int result = (int) instance.Call("DiffRange", range);
-		Assert.AreEqual(result, 2);		
+		Assert.AreEqual(result, 2);
 	}
-
+	
 	[Test]
-	public void NestedStructTest1() 
-	{	
+	public void NestedStructTest1()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
 		
 		NSRect r = (NSRect) instance.Call("GetRect");
-		Assert.AreEqual(1.0f, r.origin.x);		
-		Assert.AreEqual(2.0f, r.origin.y);		
-		Assert.AreEqual(3.0f, r.size.width);		
-		Assert.AreEqual(4.0f, r.size.height);		
+		Assert.AreEqual(1.0f, r.origin.x);
+		Assert.AreEqual(2.0f, r.origin.y);
+		Assert.AreEqual(3.0f, r.size.width);
+		Assert.AreEqual(4.0f, r.size.height);
 	}
-
+	
 	[Test]
-	public void NestedStructTest2() 
-	{	
+	public void NestedStructTest2()
+	{
 		NSObject instance = (NSObject) new Class("Subclass1").Call("alloc").Call("init");
 		
 		NSRect r = new NSRect();
@@ -302,10 +302,10 @@ public class ExportTests
 		string str = Marshal.PtrToStringAuto((IntPtr) istr.Call("UTF8String"));
 		Assert.AreEqual("1234", str);
 	}
-
+	
 	[Test]
-	public void IVarTest2() 
-	{	
+	public void IVarTest2()
+	{
 		Subclass1 instance = Subclass1.makeDefault();
 		
 		// Can set ivars using IVar.
@@ -316,6 +316,6 @@ public class ExportTests
 		// Can get ivars using IVar.
 		Assert.AreEqual("hey", instance.Data);
 	}
-
+	
 	private NSObject m_pool;
 }
